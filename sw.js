38b7911665ac2c48ad461df49d5c8694b3a38eee
renzocolombo <95050105+renzocolombo@users.noreply.github.com>
2026-04-12 18:47:50 -0300

    feat: actualizar precios, index y sw en un solo commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'huerta-urbana-1776030350288'
+const CACHE_NAME = 'huerta-urbana-1776030470328'
 const ARCHIVOS_CACHE = ['/style.css', '/script.js', '/img/favicon.png']
 
 self.addEventListener('install', e => {
