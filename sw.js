44a17cbbcbd105cc3918548ab2e982938845c752
renzo colombo <renzoalejandrocolombo@gmail.com>
2026-04-22 19:58:04 -0300

    fix: force service worker update and burst cache (v29.0)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'huerta-urbana-static-v48';
+const CACHE_NAME = 'huerta-urbana-static-v49';
 const ASSETS = [
   './',
-  './style.css?v=28.0',
-  './script.js?v=28.0',
-  './auth.js?v=24.0',
+  './style.css?v=29.0',
+  './script.js?v=29.0',
+  './auth.js?v=25.0',
   './img/favicon.png',
   './manifest.json',
   './img/icon-maskable-192.png',
