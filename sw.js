611847f50d19278de57745e94bc9c219be3a0c57
renzo colombo <renzoalejandrocolombo@gmail.com>
2026-04-22 18:59:41 -0300

    style: align mobile counter and avatar in the top-right corner (v24.0)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'huerta-urbana-static-v42';
+const CACHE_NAME = 'huerta-urbana-static-v43';
 const ASSETS = [
   './',
-  './style.css?v=23.0',
-  './script.js?v=23.0',
-  './auth.js?v=19.0',
+  './style.css?v=24.0',
+  './script.js?v=24.0',
+  './auth.js?v=20.0',
   './img/favicon.png',
   './manifest.json',
   './img/icon-maskable-192.png',
