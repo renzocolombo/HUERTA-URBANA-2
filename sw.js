d51ab15474b4e467b574b54348ca71cc8acd59cb
renzo colombo <renzoalejandrocolombo@gmail.com>
2026-04-22 21:34:33 -0300

    style: push counter and avatar to the far right margin (v35.0)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'huerta-urbana-static-v54';
+const CACHE_NAME = 'huerta-urbana-static-v55';
 const ASSETS = [
   './',
-  './style.css?v=34.0',
-  './script.js?v=34.0',
-  './auth.js?v=30.0',
+  './style.css?v=35.0',
+  './script.js?v=35.0',
+  './auth.js?v=31.0',
   './img/favicon.png',
   './manifest.json',
   './img/icon-maskable-192.png',
