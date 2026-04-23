f9d578548307b0eed5db630755d7a57a25139b60
renzo colombo <renzoalejandrocolombo@gmail.com>
2026-04-22 22:32:33 -0300

    style: reduce section header spacing for peak vertical balance (v38.0)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'huerta-urbana-static-v57';
+const CACHE_NAME = 'huerta-urbana-static-v58';
 const ASSETS = [
   './',
-  './style.css?v=37.0',
-  './script.js?v=37.0',
-  './auth.js?v=33.0',
+  './style.css?v=38.0',
+  './script.js?v=38.0',
+  './auth.js?v=34.0',
   './img/favicon.png',
   './manifest.json',
   './img/icon-maskable-192.png',
