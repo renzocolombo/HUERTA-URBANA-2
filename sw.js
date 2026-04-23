2f88f55f7b1223c89c3a36ec4a1a8274bf205758
renzo colombo <renzoalejandrocolombo@gmail.com>
2026-04-22 23:50:06 -0300

    style: fix initial load navigation offset with scroll-margin-top (v47.0)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'huerta-urbana-static-v66';
+const CACHE_NAME = 'huerta-urbana-static-v67';
 const ASSETS = [
   './',
-  './style.css?v=46.0',
-  './script.js?v=46.0',
-  './auth.js?v=42.0',
+  './style.css?v=47.0',
+  './script.js?v=47.0',
+  './auth.js?v=43.0',
   './img/favicon.png',
   './manifest.json',
   './img/icon-maskable-192.png',
